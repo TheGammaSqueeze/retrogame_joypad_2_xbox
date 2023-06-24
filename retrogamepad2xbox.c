382c6c2504dd917480665ebf1648b3385a990dc3
TheGammaSqueeze <116582950+TheGammaSqueeze@users.noreply.github.com>
2023-06-24 12:43:08 +0100

    Update retrogamepad2xbox.c to change controller ID and check for screen on
    Excuse the spaghetti code.

--- retrogamepad2xbox.c
+++ retrogamepad2xbox.c
@@ -14,6 +14,7 @@ static int get_xbox_toggle_status();
 static void set_xbox_toggle_status(int value);
 static int get_performance_mode_toggle_status();
 static void set_performance_mode_toggle_status(int value);
+static int get_screen_status();
 
 ///////////////////
 
@@ -48,7 +49,8 @@ int main(void) {
         ioctl(fd, UI_SET_KEYBIT, BTN_DPAD_RIGHT);
         ioctl(fd, UI_SET_KEYBIT, BTN_BACK);
         ioctl(fd, UI_SET_KEYBIT, BTN_MODE);
-
+        ioctl(fd, UI_SET_KEYBIT, BTN_GAMEPAD);
+		
         ioctl(fd, UI_SET_EVBIT, EV_ABS); // enable analog absolute position handling
 
         setup_abs(fd, ABS_X, -1800, 1800);
@@ -63,14 +65,35 @@ int main(void) {
         setup_abs(fd, ABS_HAT0X, -1, 1);
         setup_abs(fd, ABS_HAT0Y, -1, 1);
 
-        fprintf(stderr, "Assign virtual controller as Xbox360 Controller...\n");
+        fprintf(stderr, "Assign virtual controller as Microsoft X-Box One S Controller...\n");
+        // struct uinput_setup setup = {
+                // .name = "Xbox360 Controller",
+                // .id = {
+                        // .bustype = 0x0005,
+                        // .vendor = 0x145E,
+                        // .product = 0x128E,
+                        // .version = 0x1114,
+                // }
+        // };
+		
+		
+        // struct uinput_setup setup = {
+                // .name = "Microsoft X-Box One S pad",
+                // .id = {
+                        // .bustype = 0x0003,
+                        // .vendor = 0x045e,
+                        // .product = 0x028E,
+                        // .version = 0x0301,
+                // }
+        // };
+		
         struct uinput_setup setup = {
-                .name = "Xbox360 Controller",
+                .name = "Xbox Wireless Controller",
                 .id = {
-                        .bustype = 0x0019,
-                        .vendor = 0x145E,
-                        .product = 0x128E,
-                        .version = 0x1114,
+                        .bustype = 0x0005,
+                        .vendor = 0x045e,
+                        .product = 0x02fd,
+                        .version = 0x0003,
                 }
         };
 
@@ -79,18 +102,25 @@ int main(void) {
                 perror("UI_DEV_SETUP");
                 return 1;
         }
+		
+		// if (ioctl(fd, UI_SET_PHYS,"usb-xhci-hcd.2.auto-1/input0")) {
+                // perror("UI_SET_PHYS");
+                // return 1;
+        // }
+
+        // Unbind retrogame_joypad and rebind
+        fprintf(stderr, "Unbinding retrogame_joypad...\n");
+        send_shell_command("echo singleadc-joypad > /sys/bus/platform/drivers/singleadc-joypad/unbind");
+        send_shell_command("echo singleadc-joypad > /sys/bus/platform/drivers/singleadc-joypad/unbind");
+        sleep(3);
+		
 
         fprintf(stderr, "Create virtual controller uinput device...\n");
         if (ioctl(fd, UI_DEV_CREATE)) {
                 perror("UI_DEV_CREATE");
                 return 1;
         }
-
-        // Unbind retrogame_joypad and rebind
-        fprintf(stderr, "Unbinding retrogame_joypad...\n");
-        send_shell_command("echo singleadc-joypad > /sys/bus/platform/drivers/singleadc-joypad/unbind");
-        send_shell_command("echo singleadc-joypad > /sys/bus/platform/drivers/singleadc-joypad/unbind");
-        sleep(1);
+		
         fprintf(stderr, "Rebinding retrogame_joypad, force a failure\n");
         send_shell_command("echo singleadc-joypad > /sys/bus/platform/drivers/singleadc-joypad/bind");
         sleep(1);
@@ -110,9 +140,33 @@ int main(void) {
         // Open physical_retrogame_joypad, with exclusive access to this application only
         int physical_retrogame_joypad = open(openrgp, O_RDWR | O_NONBLOCK, S_IRUSR | S_IWUSR);
         ioctl(physical_retrogame_joypad, EVIOCGRAB, 1);
+		
+		
+		char rgpremove[1000] = "rm ";
+		strcat(rgpremove, openrgp);
+		send_shell_command(rgpremove);
+		
+		char tjpremove[1000] = "rm /dev/input/";
+		strcat(tjpremove, send_shell_command("grep -E 'Name|Handlers|Phys=' /proc/bus/input/devices | grep -A1 input/touch_joypad | grep -Eo 'event[0-9]+'"));
+		send_shell_command(tjpremove);
 
         // Define data structure to capture physical inputs
         struct input_event ie;
+
+
+
+        // Create /dev/input/event# string by using grep to get physical retrogame_joypad event number
+        char opengpio[1000] = "/dev/input/";
+        strcat(opengpio, send_shell_command("grep -E 'Name|Handlers|Phys=' /proc/bus/input/devices | grep -A1 gpio-keys/ | grep -Eo 'event[0-9]+'"));
+        fprintf(stderr, "Physical gpio_keys: %s\nReady.\n", opengpio);
+
+        // Open gpio-=keys, no exclusive access 
+        int physical_gpio_keys = open(opengpio, O_RDWR | O_NONBLOCK, S_IRUSR | S_IWUSR);
+
+        // Define data structure to capture physical inputs
+        struct input_event gpioie;
+
+
 
         // you can write events one at a time, but to save overhead we'll
         // update all of them in a single write
@@ -165,19 +219,50 @@ int main(void) {
         // Add persistence across reboots for performance mode settings
         int performancemodetoggle = get_performance_mode_toggle_status();
         set_performance_mode_toggle_status(performancemodetoggle);
+		
+		// Check for screen one
+		int screenison = 1;
 
         while (1) {
 
+
+				//Read input on volume and power buttons
+				if (count % 1000 == 0) {
+                   screenison = get_screen_status();
+
+                }
+				
+				struct input_event evgpio[25];
+                memset( & evgpio, 0, sizeof evgpio);
+                read(physical_gpio_keys, & gpioie, sizeof(struct input_event));
+				
+				
+                // Read physical gpio-keys inputs
+                if (gpioie.type == 1) { 
+				
+					screenison = 1;
+
+                        if (debug_messages_enabled == 1) {
+                                fprintf(stderr, "time:%ld.%06ld\ttype:%u\tcode:%u\tvalue:%d\n", gpioie.time.tv_sec, gpioie.time.tv_usec, gpioie.type, gpioie.code, gpioie.value);
+						}		
+						
+				}
+		
+
+///////////////////////////////////////////////////////////////////////////////
+
                 struct input_event ev[25];
                 memset( & ev, 0, sizeof ev);
-
-                read(physical_retrogame_joypad, & ie, sizeof(struct input_event));
+                read(physical_retrogame_joypad, & ie, sizeof(struct input_event));		
+				
 
                 // Read physical retrogame_joypad inputs and update our virtual gamepad inputs accordingly
                 if (ie.type != 0) {
-                        if (debug_messages_enabled == 1) {
+                        if (debug_messages_enabled == 1 && ie.value != 127) {
                                 fprintf(stderr, "time:%ld.%06ld\ttype:%u\tcode:%u\tvalue:%d\n", ie.time.tv_sec, ie.time.tv_usec, ie.type, ie.code, ie.value);
                         }
+						
+						
 
                         // L1
                         if (ie.code == 310) {
@@ -271,11 +356,17 @@ int main(void) {
 
                         // RIGHT ANALOG Y
                         if (ie.code == 2) {
-                                PHYSICAL_ABS_Z = ie.value;
+							// if (ie.value > 500) {PHYSICAL_ABS_Z = ie.value-200;}
+							// if (ie.value < -500) {PHYSICAL_ABS_Z = ie.value+200;}		
+							// if (ie.value >= -500 && ie.value <= 500) {PHYSICAL_ABS_Z = ie.value;}							
+                               PHYSICAL_ABS_Z = ie.value;
                         }
 
                         // RIGHT ANALOG X
                         if (ie.code == 5) {
+							// if (ie.value > 500) {PHYSICAL_ABS_RZ = ie.value-200;}
+							// if (ie.value < -500) {PHYSICAL_ABS_RZ = ie.value+200;}		
+							// if (ie.value >= -500 && ie.value <= 500) {PHYSICAL_ABS_RZ = ie.value;}		
                                 PHYSICAL_ABS_RZ = ie.value;
                         }
                 }
@@ -383,10 +474,17 @@ int main(void) {
                 ev[24].code = SYN_REPORT;
                 ev[24].value = 0;
 
+
+
+
+if (screenison == 1)
+{
+
                 if (write(fd, & ev, sizeof ev) < 0) {
                         perror("write");
                         return 1;
                 }
+
 
                 // Add logic for back/mode/home functionality
                 if (PHYSICAL_BTN_BACK == 1) {
@@ -396,7 +494,7 @@ int main(void) {
 
                 if (backpressed == 1 && backpresscomplete == 0 && homepresscomplete == 0) {
                         // Check if back button pressed and released quickly, send back keyevent
-                        if (PHYSICAL_BTN_BACK == 0 && backcount < 120 && backpresscomplete == 0) {
+                        if (PHYSICAL_BTN_BACK == 0 && backcount < 300 && backpresscomplete == 0) {
                                 send_shell_command("input keyevent 4");
                                 backpresscomplete = 1;
                                 homepresscomplete = 1;
@@ -408,7 +506,7 @@ int main(void) {
                                 homepresscomplete = 1;
                         }
                         // Check if back button held down with no other buttons pressed, send home keyevent
-                        if (PHYSICAL_BTN_BACK == 1 && backcount > 120 && homepresscomplete == 0) {
+                        if (PHYSICAL_BTN_BACK == 1 && backcount > 300 && homepresscomplete == 0) {
                                 send_shell_command("input keyevent 3");
                                 backpresscomplete = 1;
                                 homepresscomplete = 1;
@@ -440,7 +538,7 @@ int main(void) {
                 if (PHYSICAL_BTN_THUMBL == 1 && PHYSICAL_BTN_TL == 1 && PHYSICAL_BTN_TR == 1 && xboxtogglepresscomplete == 0) {
                         ++xboxtogglecount;
 
-                        if (xboxtogglecount > 200) {
+                        if (xboxtogglecount > 400) {
                                 if (xboxtoggle == 0) {
 
                                         set_xbox_toggle_status(1);
@@ -466,7 +564,7 @@ int main(void) {
                 if (PHYSICAL_BTN_THUMBR == 1 && PHYSICAL_BTN_TL == 1 && PHYSICAL_BTN_TR == 1 && performancemodetogglepresscomplete == 0) {
                         ++performancemodetogglecount;
 
-                        if (performancemodetogglecount > 200) {
+                        if (performancemodetogglecount > 400) {
                                 if (performancemodetoggle == 2) {
 
                                         set_performance_mode_toggle_status(0);
@@ -492,9 +590,16 @@ int main(void) {
                         performancemodetogglecount = 0;
                         performancemodetogglepresscomplete = 0;
                 }
+}
+
+
+
+
+
+
 
                 // Turn off LEDS after some time
-                if (count % 1000 == 0) {
+                if (count % 3000 == 0) {
                         send_shell_command("echo \"0\" > /sys/class/leds/sc27xx\\:blue/brightness");
                         send_shell_command("echo \"0\" > /sys/class/leds/sc27xx\\:green/brightness");
                         send_shell_command("echo \"0\" > /sys/class/leds/sc27xx\\:red/brightness");
@@ -643,4 +748,29 @@ static void set_performance_mode_toggle_status(int value) {
                 send_shell_command("su -lp 2000 -c \"am start -a android.intent.action.MAIN -e toasttext 'Power Saving Mode Activated - Hold down R3+L1+R1 to switch to max performance mode.' -n bellavita.toast/.MainActivity\"");
                 send_shell_command("setprop persist.rgp2xbox.performancemode 0");
         }
+}
+
+// Get current screen status
+static int get_screen_status() {
+
+		int screenstatus = 1;
+
+		char cmd_screen_status[100];
+
+		sprintf(cmd_screen_status, "%s" ,send_shell_command("dumpsys display | grep mScreenState"));
+		
+		if(strstr(cmd_screen_status, "mScreenState=OFF") != NULL)
+		{
+			screenstatus = 0;
+		}
+		else
+		{
+			screenstatus = 1;
+		}
+		
+        if (debug_messages_enabled == 1) {
+			fprintf(stderr, "%i", screenstatus);
+        }
+
+        return screenstatus;
 }
